8fcaf1b442513e460732b908aff2604483cc4b29
djadhav <djadhav@atlassian.com>
2021-03-04 14:57:26 -0800

    add ListItem and style for page-tree

--- page-tree-app/src/App.js
+++ page-tree-app/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
-function App() {
+import ListItem from "./ListItem";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <ListItem key="1" text="ListItem1" />
+        <ListItem key="2" text="ListItem2" />
+      </nav>
     </div>
   );
 }
-
-export default App;
